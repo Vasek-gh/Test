6ba91dbc973ddd7094d026a3e51c67ce647668e9
Vasek <my.super.g.email@gmail.com>
2025-07-08 22:40:47 +0300

    -

--- .github/scripts/commit_history.js
+++ .github/scripts/commit_history.js
@@ -3,11 +3,13 @@ module.exports = async ({core, exec, commitList}) => {
         core.info(`commitList ${commitList}`);
 
         const prList = await execCommand(`gh pr list --state closed --search "${commitList}" --json "number,url,title,closingIssuesReferences,mergeCommit"`);
-        //core.info(`prList ${prList}`);
+        const obj = JSON.parse(json);
+        core.info(`prList ${prList}`);
+        core.info(`obj ${obj}`);
 
-        for (const pr of prList) {
+        /*for (const pr of prList) {
             core.info(`found pr: ${pr.number} ${pr.title} ${pr.url}`);
-        }
+        }*/
     }
     catch(e) {
         core.setFailed(e);
