5c011db61f9530e97197c679e93d114640cfd601
Vasek <my.super.g.email@gmail.com>
2025-07-08 22:30:25 +0300

    -

--- .github/scripts/commit_history.js
+++ .github/scripts/commit_history.js
@@ -1,8 +1,27 @@
-module.exports = async ({core, prListJson}) => {
+module.exports = async ({core, commitList}) => {
     try {
-        core.info(`Json ${prListJson}`);
+        core.info(`commitList ${commitList}`);
+        commitList = commitList.split("\n").concat(" ");
+
+        const prList = execCommand(`gh pr list --state closed --search "${commitList}" --json "number,url,title,closingIssuesReferences,mergeCommit"`);
+        core.info(`prList ${prList}`);
     }
     catch(e) {
         core.setFailed(e);
+    }
+
+    async function execCommand(command) {
+        const {
+            exitCode,
+            stdout,
+            stderr
+        } = await exec.getExecOutput(command);
+
+        if (exitCode === 0) {
+            core.info(`Res ${stdout}`);
+            return stdout;
+        }
+
+        throw new Error(`The process failed with code: ${exitCode}`);
     }
 }
