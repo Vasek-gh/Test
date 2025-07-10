589cede1fd72634a2ec74db287f77f1f001596b6
Vasek <my.super.g.email@gmail.com>
2025-07-10 22:29:07 +0300

    -

--- .github/scripts/commit_history.js
+++ .github/scripts/commit_history.js
@@ -1,33 +1,47 @@
-module.exports = async ({core, exec, commitList}) => {
+module.exports = async ({core, exec, prJson}) => {
     try {
-        core.info(`commitList ${commitList}`);
+        const prList = JSON.parse(prJson);
 
-        const prList = await execCommand(`gh pr list --state closed --search "${commitList}" --json "number,url,title,closingIssuesReferences,mergeCommit"`);
-        const prListArray = JSON.parse(prList);
-        //core.info(`prList ${prList}`);
+        const lines = [];
+        lines.push(`## 2.2.2(${new Date().toISOString().split('T')[0]})`);
+        lines.push(`### Changes`);
+        lines.push(...prFilter(prList, pr => hasLabel(pr, "feature") || hasLabel(pr, "enhancement")).map(createPrRow));
+        lines.push(`### Fixes`);
+        lines.push(...prFilter(prList, pr => hasLabel(pr, "bug fix")).map(createPrRow));
 
-        //core.info(`obj ${obj}`);
-
-        for (const pr of prListArray) {
-            core.info(`found pr: ${pr.number} ${pr.title} ${pr.url}`);
-        }
+        core.info(lines.join(`\n`));
     }
     catch(e) {
         core.setFailed(e);
     }
 
-    async function execCommand(command) {
-        const {
-            exitCode,
-            stdout,
-            stderr
-        } = await exec.getExecOutput(command);
+    function prFilter(srcPrList, predicate) {
+        const result = [];
 
-        if (exitCode === 0) {
-            //core.info(`Res ${stdout}`);
-            return stdout;
+        const numberSet = new Set();
+        for (const pr of srcPrList) {
+            if (numberSet.has(pr.number) || !predicate(pr)) {
+                continue;
+            }
+
+            result.push(pr);
+            numberSet.add(pr.number);
         }
 
-        throw new Error(`The process failed with code: ${exitCode}`);
+        return result;
+    }
+
+    function createPrRow(pr) {
+        return `* ${pr.title} by [**@${pr.author.name}**](https://github.com/${pr.author.login}) in [**#${pr.number}**](${pr.url})`
+    }
+
+    function hasLabel(obj, labelPattern) {
+        for (const label of obj.labels) {
+            if (label.name?.toLowerCase() === labelPattern) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
