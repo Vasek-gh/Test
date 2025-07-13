1cea914f91e99114d2c2b89361f3dd2273ff9285
Vasek <my.super.g.email@gmail.com>
2025-07-13 09:44:51 +0300

    create_release

---
+++ .github/scripts/get_notes_info.js
@@ -0,0 +1,27 @@
+module.exports = async ({core, changelog}) => {
+    try {
+        const matches = [...changelog.matchAll("## ([0-9].[0-9].[0-9])")];
+        if (!matches) {
+            throw new Error("empty match");
+        }
+
+        const notesLength = matches.length < 2
+            ? undefined
+            : matches[1].index - matches[0].index;
+
+        const result = {
+            version: matches[0][1],
+            notes: changelog.substring(matches[0].index, notesLength)
+                .split("\n")
+                .slice(1)
+                .join("\n")
+        }
+
+        core.info(result);
+
+        return result;
+    }
+    catch(e) {
+        core.setFailed(e);
+    }
+}
